947b7a81cb359f1279c5096eb458b290e61d56b3
Brooke Ross <brooke.ross2800@gmail.com>
2022-08-31 08:09:24 -0600

    commit tree e4527d0d8a5480e8623758f01bf28f7ac598ff78

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(){
+    const returnFirstTwoDrivers= ["Antonia","Nuru","Amari","Mo",]
+    return returnFirstTwoDrivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(){
+    const returnLastTwoDrivers=['Amari','Mo']
+    return returnLastTwoDrivers.splice(-2)
+}
+
+const selectingDrivers= [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+const createFareMultiplier=function(multiply){
+    return function (num){
+        return multiply*num;
+    };
+}
+
+const fareDoubler=createFareMultiplier(2);
+const fareTripler=createFareMultiplier(3);
+
+const selectDifferentDrivers= function(drivers, driversToReturn){
+    return driversToReturn(drivers);
+}
